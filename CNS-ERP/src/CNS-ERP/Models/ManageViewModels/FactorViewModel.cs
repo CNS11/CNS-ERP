6466283084c50a7ceda05bcdfe9e8072392e3e13
Lukasz <lukaszszeszol@gmail.com>
2016-08-17 18:29:52 +0200

    initial commit

---
+++ CNS-ERP/src/CNS-ERP/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CNS_ERP.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
